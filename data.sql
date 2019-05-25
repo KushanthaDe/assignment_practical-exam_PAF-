3c8ead6ca68ce15f145f0bbdb12df8a28df8d359
KushanthaDe <40318583+KushanthaDe@users.noreply.github.com>
2019-05-25 22:40:04 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,37 @@
+
+
+insert into customers values(1,'addr1','email1','ashen','Rathnasekara','pass1','0766867936','uname1');
+
+insert into customers values(2,'addr2','email2','ashen','Rathnasekara','pass2','0766867936','uname2');
+
+insert into customers values(3,'addr3','email3','ashen','Rathnasekara','pass3','0766867936','uname3');
+
+
+
+insert into foods values(1,10,'discription','Rice',500,5);
+
+insert into foods values(2,5,'discription','Kottu',400,50);
+
+insert into foods values(3,2.5,'discription','Pizza',1500,55);
+
+insert into foods values(4,5,'discription','Hotdog',100,25);
+
+
+
+insert into orders values(1,2,56,'2019-04-25',3,457,1450);
+
+insert into orders values(2,2547,556,'2019-04-28',2,4571,800);
+
+insert into orders values(3,122,156,'2019-04-29',1,1457,300);
+
+insert into orders values(4,782,586,'2019-04-30',5,3457,2450);
+
+
+
+insert into payments values(1,25,24,1250,'2019-05-03','Paid');
+
+insert into payments values(2,825,2400,2250,'2019-05-04','Unpaid');
+
+insert into payments values(3,245,278,250,'2019-05-05','Paid');
+
+insert into payments values(4,365,3224,1000,'2019-05-06','Paid');
